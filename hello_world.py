40ce0323ac99cc9397c476c87f5b20f59a497377
Nick <koteloks@yandex.ua>
2015-11-01 22:54:22 +0200

    dfgfdgfg

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,4 @@ __author__ = 'admin'
 print("Hello world")
 print ("another test")
 # dsfshgjghfdgfdgfdgfdgfgx`xasdsadsadsfdsfqwqdwq
-print ("changes123213fgdgfd")
+print ("changes123213fgdgfdasdasdas")
